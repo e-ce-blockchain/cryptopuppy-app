ee07f1e3c22e01333154fd8d1d1cf206be453a93
elenapan <elenapan@protonmail.com>
2021-05-19 15:05:48 +0300

    Updates

---
+++ precache-manifest.14735f67dfa0cfd5c8ebb9cce84e2f00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771bc5f1c8ab208e6f7f40b435089ddf",
+    "url": "/cryptopuppy-app/index.html"
+  },
+  {
+    "revision": "54f674278e80c7477ca4",
+    "url": "/cryptopuppy-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "672c016cefbe82caa423",
+    "url": "/cryptopuppy-app/static/js/2.4134afaa.chunk.js"
+  },
+  {
+    "revision": "54f674278e80c7477ca4",
+    "url": "/cryptopuppy-app/static/js/main.97835061.chunk.js"
+  },
+  {
+    "revision": "8bb24d641a5ee6966c42",
+    "url": "/cryptopuppy-app/static/js/runtime-main.8f8414d9.js"
+  }
+]);
